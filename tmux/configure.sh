8df12361a8e35674ae945e7cdf915071cc53b7b1
Jake Rogers <code@supportoss.org>
2023-08-23 21:42:00 -0600

    tmux/configure.sh - changed tmux to full install mode only and added tmux plugin manager

--- tmux/configure.sh
+++ tmux/configure.sh
@@ -2,4 +2,13 @@
 
 set -e
 
-ln -sf "${DOTFILES_LOCATION}/tmux/tmux.conf" "${HOME}/.tmux.conf"
+if [ "${INSTALL_MODE}" = 'full' ]; then
+  ln -sf "${DOTFILES_LOCATION}/tmux/tmux.conf" "${HOME}/.tmux.conf"
+
+  # install tmux plugin manager
+  mkdir -p ~/.tmux/plugins
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+  # install tmux plugins
+  ~/.tmux/plugins/tpm/bin/install_plugins
+fi

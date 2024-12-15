2569ba499a95fbdead82492678a162be3d1bf1ac
Jake Rogers <code@supportoss.org>
2024-12-14 19:04:44 -0700

    feat(omz): add function to install/upgrade kubectl

--- omz/functions.zsh
+++ omz/functions.zsh
@@ -147,6 +147,40 @@ function git-tag-semver() {
 
 
 #######################################
+# download and install kubectl
+# Arguments:
+#   $1 - the version of kubectl to install, defaults to the latest version
+#######################################
+function install-kubectl() {
+  local version=""
+  # handle -h or --help flags
+  if [[ $1 == "--help" || $1 == "-h" || $1 == "-?" ]]; then
+    echo "Usage: install-kubectl [version]"
+    echo "Installs the specified version of kubectl, or the latest version if no version is specified"
+    return
+  elif [ -n "$1" ]; then
+    # test if the version is valid
+    if [[ "$1" =~ '^[0-9]\.[0-9]{2}$' ]]; then
+      version="$(/usr/bin/curl -L -s https://dl.k8s.io/release/stable-${1}.txt)"
+    else
+      pprint "🔴 Invalid version format. Please use the format X.YY" $fgRed
+      return
+    fi
+  elif [ -z "$1" ]; then
+    version="$(/usr/bin/curl -L -s https://dl.k8s.io/release/stable.txt)"
+  fi
+
+  /usr/bin/curl  -o ~/install/kubectl -L "https://dl.k8s.io/release/${version}/bin/linux/amd64/kubectl"
+  if [ $? -ne 0 ]; then
+    pprint "🔴 Failed to download kubectl ${version}" $fgRed
+    return
+  fi
+  sudo /usr/bin/install -o root -g root -m 0755 ~/install/kubectl /usr/local/bin/kubectl
+  [ $? -eq 0 ] && pprint "🟢 Installed kubectl ${version}" $fgGreen
+}
+
+
+#######################################
 # join two csv files on the first column. The first row of each file is assumed to be the header.
 # Arguments:
 #   $1 - 'file1' the first csv file

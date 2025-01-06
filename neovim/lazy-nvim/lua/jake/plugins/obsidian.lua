2bd7dc63266831b4ffcc966091a3193bbc7144d2
Jake Rogers <code@supportoss.org>
2025-01-05 20:07:26 -0700

    fix(nvim): add obsidian plugin

---
+++ neovim/lazy-nvim/lua/jake/plugins/obsidian.lua
@@ -0,0 +1,148 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "3.*", -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = "markdown",
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   -- refer to `:h file-pattern` for more examples
+  --   "BufReadPre path/to/my-vault/*.md",
+  --   "BufNewFile path/to/my-vault/*.md",
+  -- },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+
+  opts = {
+    -- A list of workspace names, paths, and configuration overrides.
+    -- If you use the Obsidian app, the 'path' of a workspace should generally be
+    -- your vault root (where the `.obsidian` folder is located).
+    -- When obsidian.nvim is loaded by your plugin manager, it will automatically set
+    -- the workspace to the first workspace in the list whose `path` is a parent of the
+    -- current markdown file being edited.
+    workspaces = (function()
+      local Path = require("plenary.path")
+      local my_workspaces = {}
+
+      if Path:new("/mnt/WORK/obsidian"):exists() then
+        table.insert(my_workspaces, { path = "/mnt/WORK/obsidian", name = "personal" })
+      end
+
+      if Path:new("~/documents/documentation"):exists() then
+        table.insert(my_workspaces, { path = "~/documents/documentation", name = "work" })
+      end
+
+      return my_workspaces
+    end)(),
+
+    -- Optional, set the log level for obsidian.nvim. This is an integer corresponding to one of the log
+    -- levels defined by "vim.log.levels.*".
+    log_level = vim.log.levels.INFO,
+
+    daily_notes = {
+      -- Optional, if you keep daily notes in a separate directory.
+      -- folder = "notes/dailies",
+      -- Optional, if you want to change the date format for the ID of daily notes.
+      date_format = "%Y-%m-%d",
+      -- Optional, if you want to change the date format of the default alias of daily notes.
+      alias_format = "%B %-d, %Y",
+      -- Optional, default tags to add to each new daily note created.
+      default_tags = { "daily-notes" },
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      template = nil,
+    },
+
+    -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
+    completion = {
+      -- Set to false to disable completion.
+      nvim_cmp = true,
+      -- Trigger completion at 2 chars.
+      min_chars = 2,
+    },
+
+    mappings = {
+      -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+      ["gf"] = {
+        action = function()
+          return require("obsidian").util.gf_passthrough()
+        end,
+        opts = { noremap = false, expr = true, buffer = true },
+      },
+      -- Toggle check-boxes.
+      ["<leader>oh"] = {
+        action = function()
+          return require("obsidian").util.toggle_checkbox()
+        end,
+        opts = { buffer = true },
+      },
+      -- Smart action depending on context, either follow link or toggle checkbox.
+      ["<leader>or"] = {
+        action = function()
+          return require("obsidian").util.smart_action()
+        end,
+        opts = { buffer = true, expr = true },
+      },
+    },
+
+    -- Where to put new notes. Valid options are
+    --  * "current_dir" - put new notes in same directory as the current buffer.
+    --  * "notes_subdir" - put new notes in the default notes subdirectory.
+    new_notes_location = "current_dir",
+
+    -- Either 'wiki' or 'markdown'.
+    preferred_link_style = "wiki",
+
+    -- Optional, boolean or a function that takes a filename and returns a boolean.
+    -- `true` indicates that you don't want obsidian.nvim to manage frontmatter.
+    disable_frontmatter = false,
+
+    -- Optional, for templates (see below).
+    templates = {
+      folder = "templates",
+      date_format = "%Y-%m-%d",
+      time_format = "%H:%M",
+      -- A map for custom variables, the key should be the variable and the value a function
+      substitutions = {},
+    },
+
+    picker = {
+      -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+      name = "fzf-lua",
+      -- Optional, configure key mappings for the picker. These are the defaults.
+      -- Not all pickers support all mappings.
+      note_mappings = {
+        -- Create a new note from your query.
+        new = "<C-x>",
+        -- Insert a link to the selected note.
+        insert_link = "<C-l>",
+      },
+      tag_mappings = {
+        -- Add tag(s) to current note.
+        tag_note = "<C-x>",
+        -- Insert a tag at the current location.
+        insert_tag = "<C-l>",
+      },
+    },
+
+    -- Optional, sort search results by "path", "modified", "accessed", or "created".
+    -- The recommend value is "modified" and `true` for `sort_reversed`, which means, for example,
+    -- that `:ObsidianQuickSwitch` will show the notes sorted by latest modified time
+    sort_by = "modified",
+    sort_reversed = true,
+
+    -- Set the maximum number of lines to read from notes on disk when performing certain searches.
+    search_max_lines = 1000,
+
+    -- Optional, determines how certain commands open notes. The valid options are:
+    -- 1. "current" (the default) - to always open in the current window
+    -- 2. "vsplit" - to open in a vertical split if there's not already a vertical split
+    -- 3. "hsplit" - to open in a horizontal split if there's not already a horizontal split
+    open_notes_in = "current",
+
+    ui = {
+      enable = false, -- set to false to disable all additional syntax features
+    },
+  },
+}

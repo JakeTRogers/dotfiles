b05c3f09d499013ec4ea1aeb90bccea0a75e9245
Jake Rogers <code@supportoss.org>
2026-01-31 16:23:14 -0700

    fix(bat): remove duplicate symlink for bat themes in configure script

--- bat/configure.sh
+++ bat/configure.sh
@@ -3,9 +3,8 @@
 set -e
 
 ln -snf "${DOTFILES_LOCATION}/bat/config" "${HOME}/.config/bat"
-ln -sf "$DOTFILES_LOCATION/bat/config/themes" ~/.config/bat/themes
 
 # Build bat theme cache so custom themes are available
 if command -v bat &>/dev/null; then
-	bat cache --build
+  bat cache --build
 fi
